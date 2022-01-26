769102b2e1501697b751effa8137c4e853108ecf
Biafcampos <alinecarter53@gmail.com>
2022-01-26 17:59:27 -0300

    putting the check

--- contagem.js
+++ contagem.js
@@ -1,3 +1,20 @@
 let numeroAlunos = 10;
 
-for(let contador = 0; contador < numeroAlunos; contador++)
+for(let contador = 0; contador < numeroAlunos; contador++){
+    
+
+
+    if(contador == 0){
+
+        console.log("O numero é ZERO.")
+        
+    } else if(contador % 2 == 0){
+        
+        console.log(`O numero ${contador} é PAR.`)
+
+    } else if(contador % 2 == 1){
+
+        console.log(`O numero ${contador} é IMPAR.`)
+
+    }
+}
